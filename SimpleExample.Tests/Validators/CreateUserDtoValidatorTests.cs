69e16eb2f8898fb8db0c6947deed83c935cc6e27
Aapo Kontiainen <faako005@edu.xamk.fi>
2026-02-10 14:12:03 +0200

    Fix test

--- SimpleExample.Tests/Validators/CreateUserDtoValidatorTests.cs
+++ SimpleExample.Tests/Validators/CreateUserDtoValidatorTests.cs
@@ -25,10 +25,8 @@ public void Should_Have_Error_When_FirstName_Is_Empty()
 {
     CreateUserDto dto = new CreateUserDto { FirstName = "", LastName = "Meikäläinen", Email = "test@test.com" };
     var result = _validator.TestValidate(dto);
-    
-    // VÄÄRÄ ODOTUS - testi epäonnistuu!
-    result.ShouldNotHaveAnyValidationErrors();
-
+    result.ShouldHaveValidationErrorFor(x => x.FirstName)
+          .WithErrorMessage("Etunimi on pakollinen");
 }
 }
 
