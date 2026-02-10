c95b611438b17ae671f22d1a20619675517492fd
Aapo Kontiainen <faako005@edu.xamk.fi>
2026-02-10 13:55:13 +0200

    Test failing tests in CI

---
+++ SimpleExample.Tests/Validators/CreateUserDtoValidatorTests.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+using FluentValidation;
+using SimpleExample.Domain.Entities;
+using SimpleExample.Application.DTOs;
+using Xunit;
+
+namespace SimpleExample.Tests.Validators;
+
+public class ValidatorTests
+{
+  private readonly IValidator<CreateUserDto> _validator;
+
+  public ValidatorTests()
+  {
+    var v = new InlineValidator<CreateUserDto>();
+    v.RuleFor(x => x.FirstName)
+     .NotEmpty().WithMessage("Etunimi on pakollinen");
+
+    _validator = v;
+  }
+
+  [Fact]
+  public void Should_Have_Error_When_FirstName_Is_Empty()
+  {
+    CreateUserDto dto = new CreateUserDto { FirstName = "", LastName = "Meikäläinen", Email = "test@test.com" };
+    var result = _validator.Validate(dto);
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().Contain(f => f.PropertyName == "FirstName" && f.ErrorMessage == "Etunimi on pakollinen");
+  }
+}
+
